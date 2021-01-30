124df5ad1070517b04016d43a7655195f0105f83
leventenyiro <nyiro.levente@gmail.com>
2021-01-30 12:11:19 +0100

    changes

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+let port = process.env.PORT || 8080
+
+app.get("/", (req, res) => {
+    res.send("Hello")
+})
 
 app.get("/test", (req, res) => {
     res.send("test")
 })
 
-app.listen(8080, () => {
-    console.log("Server is running on port 8080...")
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}...`)
 })
